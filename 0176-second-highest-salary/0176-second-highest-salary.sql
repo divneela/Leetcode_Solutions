5a1ff91f8e505a9c217a742a9750d0185e9da950
Divya Neelamegam <142276385+divneela@users.noreply.github.com>
2023-12-22 16:15:52 -0800

    Time: 400 ms (92.00%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -2,5 +2,6 @@
 
 select coalesce(
     (select salary from 
-     (select id, salary, dense_rank() over ( ORDER BY salary desc) as rn from Employee ) a      where rn=2 limit 1)
+     (select id, salary, DENSE_RANK() over ( ORDER BY salary desc) as rn from Employee ) a      where rn=2 limit 1)
     , null) as SecondHighestSalary
+    
